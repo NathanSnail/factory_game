c45820ef9a874f444dca052a030cca9a9199e153
Nathan <nathan.titirangi@gmail.com>
2024-06-24 18:53:31 +1200

    type system is somewhat interesting

--- main.go
+++ main.go
@@ -5,14 +5,46 @@ import "fmt"
 const W = 64
 const H = 64
 
-type Board [W][H]CellPair
+type Board [W][H]ICell
+
+func (b Board) requestRun(pos Vec2) {
+	if !b.contains(pos) {
+		return
+	}
+	v := b.get(pos)
+	if v.updated {
+		return
+	}
+	v.updated = true
+	v.cell.update(&b, pos)
+}
+func (b Board) get(pos Vec2) *ICell {
+	return &b[pos.x][pos.y]
+}
+
+func (b Board) set(pos Vec2, v ICell) {
+	b[pos.x][pos.y] = v
+}
+
+func (board *Board) tryPushTo(value *int, dir Vec2, pos Vec2) bool {
+	sampling := pos.add(dir)
+	if !board.contains(sampling) {
+		return false
+	}
+	v := board.get(pos).value
+	if v == nil {
+		board.get(pos).value = value
+		return true
+	}
+	return false
+}
 
 type Vec2 struct {
 	x int
 	y int
 }
 
-func boardContains(v Vec2) bool {
+func (Board) contains(v Vec2) bool {
 	return v.x >= 0 && v.x < W && v.y >= 0 && v.y < H
 }
 
@@ -20,14 +52,15 @@ func (v1 Vec2) add(v2 Vec2) Vec2 {
 	return Vec2{x: v1.x + v2.x, y: v2.y + v2.y}
 }
 
-type CellPair struct {
-	value *int
-	cell  Cell
+type ICell struct {
+	value   *int
+	cell    Cell
+	updated bool
 }
 
 type Cell interface {
 	symbol() string
-	update(board *Board, pos Vec2) int
+	update(board *Board, pos Vec2)
 }
 type Direction int8
 
@@ -42,8 +75,8 @@ type Conveyor struct {
 	dir Direction
 }
 
-func (c Conveyor) symbol() string {
-	switch c.dir {
+func directionalArrow(d Direction) string {
+	switch d {
 	case Left:
 		return "<"
 	case Right:
@@ -53,24 +86,15 @@ func (c Conveyor) symbol() string {
 	case Down:
 		return "v"
 	}
-	panic("Invalid conveyor")
+	panic("Invalid direction")
 }
 
-func tryPushTo(board *Board, value *int, dir Vec2, pos Vec2) bool {
-	sampling := pos.add(dir)
-	if !boardContains(sampling) {
-		return false
-	}
-	v := board[sampling.x][sampling.y].value
-	if v == nil {
-		board[sampling.x][sampling.y].value = value
-		return true
-	}
-	return false
+func (c Conveyor) symbol() string {
+	return directionalArrow(c.dir)
 }
 
-func (c Conveyor) getDir() Vec2 {
-	switch c.dir {
+func getDirVec(dir Direction) Vec2 {
+	switch dir {
 	case Left:
 		return Vec2{x: -1, y: 0}
 	case Right:
@@ -80,21 +104,46 @@ func (c Conveyor) getDir() Vec2 {
 	case Down:
 		return Vec2{x: 0, y: 1}
 	}
-	panic("Invalid conveyor")
+	panic("Invalid direction")
 }
 
 func (c Conveyor) update(board *Board, pos Vec2) {
-	cur := board[pos.x][pos.y].value
+	cur := board.get(pos).value
 	if cur == nil {
 		return
 	}
-	success := tryPushTo(board, cur, c.getDir(), pos)
+	success := board.tryPushTo(cur, getDirVec(c.dir), pos)
 	if success {
-		board[pos.x][pos.y].value = nil
+		board.get(pos).value = nil
 	}
 }
 
+type Generator struct {
+	dir   Direction
+	value int
+}
+
+func (g Generator) symbol() string {
+	return "\033[36m" + directionalArrow(g.dir)
+}
+
+func (g Generator) update(board *Board, pos Vec2) {
+	board.tryPushTo(&g.value, getDirVec(g.dir), pos)
+}
+
+type Empty struct{}
+
+func (Empty) symbol() string      { return " " }
+func (Empty) update(*Board, Vec2) {}
+
 func main() {
+	board := Board{}
+	for x := 0; x < W; x++ {
+		for y := 0; y < H; y++ {
+			board.set(Vec2{x: x, y: y}, ICell{updated: false, value: nil, cell: Empty{}})
+		}
+	}
+	board.set(Vec2{x: 0, y: 0}, ICell{updated: false, value: nil, cell: Generator{dir: Down, value: 1}})
 	fmt.Println("test!")
 	a := 10
 	fmt.Println(a)
